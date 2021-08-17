ea3ee8ae4202d815c1fafbd7380dacb258d0ee61
TienPhan4363 <phanthaitien4363@gmail.com>
2021-08-17 23:40:20 +0700

    Design trello front end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,103 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <nav className="navbar app">Navbar App</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brain Storm</header>
+          <ul>
+            <li>
+            <img src="https://trungquandev.com/wp-content/uploads/2021/06/mern-stack-trungquandev-youtube-thumnail-awesome-course.png" 
+                alt="An image"></img>
+              Title: This is an image
+            </li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brain Storm</header>
+          <ul>
+            <li>
+            <img src="https://trungquandev.com/wp-content/uploads/2021/06/mern-stack-trungquandev-youtube-thumnail-awesome-course.png" 
+                alt="An image"></img>
+              Title: This is an image
+            </li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brain Storm</header>
+          <ul>
+            <li>
+            <img src="https://trungquandev.com/wp-content/uploads/2021/06/mern-stack-trungquandev-youtube-thumnail-awesome-course.png" 
+                alt="An image"></img>
+              Title: This is an image
+            </li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brain Storm</header>
+          <ul>
+            <li>
+            <img src="https://trungquandev.com/wp-content/uploads/2021/06/mern-stack-trungquandev-youtube-thumnail-awesome-course.png" 
+                alt="An image"></img>
+              Title: This is an image
+            </li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      
+        <div className="column">
+          <header>Brain Storm</header>
+          <ul>
+            <li>
+            <img src="https://trungquandev.com/wp-content/uploads/2021/06/mern-stack-trungquandev-youtube-thumnail-awesome-course.png" 
+                alt="An image"></img>
+              Title: This is an image
+            </li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+            <li>Add whatever you like!</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+      </div>
     </div>
   );
 }
